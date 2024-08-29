829662bda10b5693e8c0e3bac9991f69ecd27c12
Kang-1230 <tnwls7138@gmail.com>
2024-08-29 09:39:02 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.15 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.js
@@ -5,20 +5,7 @@ function solution(numbers) {
     for(let i = 0; i<completeArr.length; i++){
         if (Boolean(numbers.find((e) => e === completeArr[i])) === false){
             sum = sum + completeArr[i]
-            // newArr.push(completeArr[i])}
     }}
         return sum;
-    
-        // var filteredArr = completeArr.filter((num)=>{return num !== numbers[i]})
-        // 
-        // console.log(filteredArr);
     }
     
-    
-//     var answer = -1;
-//     return answer;
-
-
-//0-9까지 새 배열 하나 만들고
-//numbers와 같은 요소 삭제하고 for문 안에서 filter(() => numbers === arr))
-//나머지 더하면 될듯
